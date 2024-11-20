167440e43162118889b2c04e0de2ea39ac2f2a54
lagecode <lagecode@outlook.com>
2024-11-20 18:27:40 +0100

    Commit scripts and exclude .tar

--- export-data.sh
+++ export-data.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
+#!/bin/bash 
 
-source .env
+# Run the check_docker_compose.sh script 
+./docker-status.sh 
 
-if [ "$(docker ps -q -f name=$DB_CONTAINER)" ]; then 
-    echo "Shutting down $DB_CONTAINER before data export."  
+# Capture the exit status 
+STATUS=$?
+
+# Check the status and perform actions accordingly 
+if [ $STATUS -eq 0 ]; then 
+    docker-compose stop
 fi
 
-docker run --rm -v hardsail_odoo-db-data:/data -v ./backup:/backup ubuntu tar cvf /backup/odoo-db-data.tar /data
-docker run --rm -v hardsail_odoo-web-data:/data -v ./backup:/backup ubuntu tar cvf /backup/odoo-web-data.tar /data
+docker run --rm -v hardsail_odoo-db-data:/data -v ./backup:/backup ubuntu tar cvf /backup/odoo-db-data.tar -o /data
+docker run --rm -v hardsail_odoo-web-data:/data -v ./backup:/backup ubuntu tar cvf /backup/odoo-web-data.tar -o /data
 
+sudo chown $(whoami):$(whoami) ./backup ./backup/*
+
+gzip ./backup/*
+
+git add ./backup
