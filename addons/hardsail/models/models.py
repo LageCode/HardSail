5a2bd2a7278f61c0bef76c6c86a4c4eaa8c27e22
lagecode <lagecode@outlook.com>
2025-01-11 15:00:33 +0100

    propertie values show in product creation view

--- addons/hardsail/models/models.py
+++ addons/hardsail/models/models.py
@@ -66,18 +66,72 @@ class ProductTemplate(models.Model):
     is_hardware = fields.Boolean(string='Is a hardware component', default=False)
     component_id = fields.Many2one('component.template', string='Component Template')
     property_ids = fields.Many2many(related='component_id.property_ids', string='Properties', readonly=True)
+    property_value_ids = fields.One2many('product.property.value', 'product_tmpl_id', string='Property Values')
+    
+    def _sync_property_values(self):
+        """Sync property values with component template"""
+        self.ensure_one()
+        existing_props = {pv.property_id.id: pv for pv in self.property_value_ids}
+        
+        values = []
+        if self.component_id:
+            for prop in self.component_id.property_ids:
+                if prop.id not in existing_props:
+                    values.append((0, 0, {
+                        'property_id': prop.id,
+                    }))
+            
+            # Remove properties that don't exist in new component
+            for prop_id in existing_props:
+                if prop_id not in self.component_id.property_ids.ids:
+                    values.append((2, existing_props[prop_id].id, 0))
+                    
+        if not self.component_id:
+            values = [(5, 0, 0)]  # Clear all if no component
+            
+        if values:
+            self.property_value_ids = values
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for record in records:
+            if record.is_hardware and record.component_id:
+                record._sync_property_values()
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'component_id' in vals:
+            for record in self:
+                if record.is_hardware:
+                    record._sync_property_values()
+        return res
+
+    @api.onchange('component_id')
+    def _onchange_component_id(self):
+        if self.is_hardware:
+            self._sync_property_values()
 
 
-# class hardsail(models.Model):
-#     _name = 'hardsail.hardsail'
-#     _description = 'hardsail.hardsail'
+class ProductPropertyValue(models.Model):
+    _name = 'product.property.value'
+    _description = 'Product Property Value'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    product_tmpl_id = fields.Many2one('product.template', string='Product Template', required=True, ondelete='cascade')
+    property_id = fields.Many2one('component.property', string='Property', required=True)
+    
+    string_value = fields.Char(string='String Value')
+    integer_value = fields.Integer(string='Integer Value')
+    float_value = fields.Float(string='Float Value')
+    boolean_value = fields.Boolean(string='Boolean Value')
+    selection_value = fields.Many2one('component.property.option', string='Selection Value')
+
+    @api.onchange('property_id')
+    def _onchange_property_id(self):
+        # Clear all values when property changes
+        self.string_value = False
+        self.integer_value = False
+        self.float_value = False
+        self.boolean_value = False
+        self.selection_value = False
